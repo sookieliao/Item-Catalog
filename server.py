11595c9e4199bf962404e630638050570b9349cb
= <asd6629827@gmail.com>
2019-09-10 20:04:13 -0700

    added display error message functionality too createUser page. Add API calls to return JSON data.

--- server.py
+++ server.py
@@ -16,41 +16,40 @@ import random, string
 
 loggedIn = False
 
+@app.route('/sookiesusedcameras/newuser/<messages>', methods=['GET','POST'])
+def createUser(messages):
+    inputs = {}
+    if messages == "DEFAULT":
+        messages = None
 
-def getUserInfo(user_id):
-    session = DBSession()
-    user = session.query(User).filter_by(id=user_id).one
-    return user
-
-@app.route('/sookiesusedcameras/newuser', methods=['GET','POST'])
-def createUser():
     if request.method == 'GET':
-        return render_template('createUser.html')
+        inputs['messages']=messages
+        return render_template('createUser.html', inputs=inputs)
     else:
         session = DBSession()
-        if request.form['password1'] == request.form['password2']:
-            print "passwords are the same. now adding user..."
+
+        if not request.form['name']:
+            inputs['messages']= "Username is required. Please fill it up."
+            return redirect(url_for('createUser', messages=inputs['messages']))
+        elif not request.form['email']:
+            inputs['messages']= "Email is required. Please fill it up."
+            return redirect(url_for('createUser', messages=inputs['messages']))
+
+        elif request.form['password1'] != request.form['password2']:
+            inputs['messages']= "Password doesn't match. Please try again."
+            return redirect(url_for('createUser', messages=inputs['messages']))
+        else:
             user = User(name=request.form['name'],email=request.form['email'],password=request.form['password1'])
             session.add(user)
             session.commit()
-            print "User created successfully with user_id %s!"% user.id
             loggedIn = True
             login_session['username']=user.name
             login_session['id']=user.id
             login_session['email']=user.email
             login_session['currentUser'] = login_session['username']
+            login_session['loggedIn'] = True
             flash("you are now logged in as %s" % login_session['username'])
-
-            users = session.query(User).all()
-            for i in users:
-                print i.name
-                print i.id
-                print i.email
-
-            print "Redirecting to my store..."
             return redirect(url_for('showMyStore', user_id=user.id))
-        else:
-            print "Password doesn't match. Please try again."
 
 
 @app.route('/sookiesusedcameras/login/message/<messages>', methods=['GET','POST'])
@@ -62,12 +61,9 @@ def login(messages):
 
     if request.method == 'GET':
         inputs['messages']=messages
-        print inputs['messages']
         return render_template('login.html', inputs=inputs)
     else:
-        print 'authendicating user...'
         session = DBSession()
-        print request.form
         user = session.query(User).filter_by(email=request.form['email']).first()
 
         if not user :
@@ -83,43 +79,33 @@ def login(messages):
             return redirect(url_for('login', messages=inputs['messages']))
 
         else:
-            print "Login success. Redirecting to my store..."
             loggedIn = True
             login_session['username']=user.name
             login_session['id']=user.id
             login_session['email']=user.email
             login_session['currentUser']  = login_session['username']
             flash("you are now logged in as %s" % login_session['username'])
-            print "done!"
             return redirect(url_for('showMyStore', user_id=user.id))
 
 @app.route('/sookiesusedcameras/logout')
 def logout():
-    print 'logging out...'
     del login_session['currentUser']
     del login_session['username']
     del login_session['email']
     del login_session['id']
     flash("you are now logged out.")
     loggedIn = False
-    print "done!"
     return redirect(url_for('showCameras'))
-
 
 @app.route('/')
 @app.route('/sookiesusedcameras')
 def showCameras():
-    print 'home'
-    print 'login session: %s' % login_session
     inputs = {}
     inputs['loggedIn'] = False
-
-    print login_session
 
     if "currentUser" in login_session :
         inputs['loggedIn'] = True
         inputs['user_id']=login_session['id']
-        #get user id
 
     session = DBSession()
     cameras = session.query(Camera).all()
@@ -147,8 +133,6 @@ def showCameras():
 
 @app.route('/sookiesusedcameras/brand/<int:brand_id>')
 def showCamerasWithBrand(brand_id):
-    print 'brands'
-    print 'login session: %s' % login_session
     inputs = {}
     inputs['loggedIn'] = False
 
@@ -165,7 +149,6 @@ def showCamerasWithBrand(brand_id):
         inputs['user_id']= login_session['id']
     else:
         inputs['user_id']= 0
-    print 'current user id: %s'%str(inputs['user_id'])
 
     inputs['targetBrand'] = session.query(Brand).filter_by(id=brand_id).one()
 
@@ -186,8 +169,6 @@ def showCamerasWithBrand(brand_id):
 
 @app.route('/sookiesusedcameras/category/<int:category_id>')
 def showCamerasWithCategory(category_id):
-    print 'category'
-    print 'login session: %s' % login_session
     inputs = {}
     inputs['loggedIn'] = False
 
@@ -204,7 +185,6 @@ def showCamerasWithCategory(category_id):
         inputs['user_id']= login_session['id']
     else:
         inputs['user_id']= 0
-    print 'current user id: %s'%str(inputs['user_id'])
 
     inputs['targetCategory'] = session.query(Category).filter_by(id=category_id).one()
 
@@ -227,8 +207,6 @@ def showCamerasWithCategory(category_id):
 
 @app.route('/sookiesusedcameras/use/<int:user_id>/newcamera', methods=['GET','POST'])
 def addItem(user_id):
-    print 'add item'
-    print 'login session: %s' % login_session
     inputs = {}
     if "currentUser" in login_session :
         inputs['loggedIn'] = True
@@ -250,8 +228,6 @@ def addItem(user_id):
 @app.route('/sookiesusedcameras/user/<int:user_id>')
 def showMyStore(user_id):
     inputs = {}
-    print 'showMyStore'
-    print 'login session: %s' % login_session
 
     if "currentUser" in login_session :
         inputs['loggedIn'] = True
@@ -280,7 +256,6 @@ def showMyStore(user_id):
         c_id_name_pair[str(c.id)]=c.name
     inputs['categories']=categories
     inputs['c_id_name_pair']=c_id_name_pair
-
 
     return render_template('showMyStore.html',inputs=inputs)
 
@@ -336,23 +311,33 @@ def deleteItem(user_id, camera_id):
 # These are for API calls that returns JSON file
 @app.route('/sookiesusedcameras/JSON')
 def getCameras():
-    return "this page will show json for all products."
+    session = DBSession()
+    cams = session.query(Camera).all()
+    return jsonify(cameras=[r.serialize for r in cams])
 
 @app.route('/sookiesusedcameras/brand/<brand_id>/JSON')
-def getCamerasWithBrand(brand_name):
-    return "this page will show json for all products for a specific brand."
+def getCamerasWithBrand(brand_id):
+    session = DBSession()
+    cams = session.query(Camera).filter_by(brand_id=brand_id).all()
+    return jsonify(cameras=[r.serialize for r in cams])
 
 @app.route('/sookiesusedcameras/category/<category_id>/JSON')
-def getCamerasWithCondition(condition):
-    return "this page will show json for all products for a condition."
+def getCamerasWithCategory(category_id):
+    session = DBSession()
+    cams = session.query(Camera).filter_by(category_id=category_id).all()
+    return jsonify(cameras=[r.serialize for r in cams])
 
 @app.route('/sookiesusedcameras/user/<int:user_id>/JSON')
 def getMyStore(user_id):
-    return 'This is the page return JSON for my store'
+    session = DBSession()
+    cams = session.query(Camera).filter_by(user_id=user_id).all()
+    return jsonify(cameras=[r.serialize for r in cams])
 
 @app.route('/sookiesusedcameras/user/<int:user_id>/camera/<int:camera_id>/JSON')
 def getMyCamera(user_id, camera_id):
-    return 'This is the page return JSON for a camera'
+    session = DBSession()
+    cams = session.query(Camera).filter_by(user_id=user_id).all()
+    return jsonify(camera=[c.serialize for c in cams if c.id==camera_id])
 
 
 if __name__ == '__main__':
